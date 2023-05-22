e6588ba0a6c19dc0cc6e773b4c2937ff8ce5b450
Samuel Faber <samuel.faber@live.fr>
2023-05-22 12:00:18 +0200

    Préparatif fait !

--- movieHandlers.js
+++ movieHandlers.js
@@ -42,13 +42,19 @@ const getMovies = (req, res) => {
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
 
-  const movie = movies.find((movie) => movie.id === id);
-
-  if (movie != null) {
-    res.json(movie);
-  } else {
-    res.status(404).send("Not Found");
-  }
+  database
+    .query("select * from movies where id = ?", [id])
+    .then(([movie]) => {
+      if (movie[0] != null) {
+        res.json(movie[0]);
+      } else {
+        res.status(404).send(`Movie N°${id} not Found`);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Error retrieving data from database`);
+    });
 };
 
 module.exports = {

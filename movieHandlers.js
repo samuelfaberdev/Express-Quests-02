1179b7114a5ab64c14f789843606d7af697b710c
Samuel Faber <samuel.faber@live.fr>
2023-05-22 11:44:18 +0200

    Première requête SQL dans movieHandlers.js

--- movieHandlers.js
+++ movieHandlers.js
@@ -25,8 +25,18 @@ const movies = [
   },
 ];
 
+const database = require("./database");
+
 const getMovies = (req, res) => {
-  res.json(movies);
+  database
+    .query("select * from movies")
+    .then(([movies]) => {
+      res.json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
 };
 
 const getMovieById = (req, res) => {
